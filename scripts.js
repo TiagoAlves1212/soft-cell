7ecb6539c564fb153863f760b8bc261c97ada02f
Tiago Alves <tiago2018cel123@gmail.com>
2025-03-01 03:24:09 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const script_do_google =
-  "https://script.google.com/macros/s/AKfycbygvcxnc7b3OU5ecZL9jzfLm4cMYH-WWrPy_YlgIrQgrWEzc2i8lolbvDOFvz0ZIGBk/exec";
+  "https://script.google.com/macros/s/AKfycbzQOZjIL4amt1eJOHfpwiXfbF-IXuczcbGcNDRBgeAgbKOi3DZHi3p8myBViuDSzNwX/exec";
 const form = document.forms["form-product"];
 
 form.addEventListener("submit", (e) => {
