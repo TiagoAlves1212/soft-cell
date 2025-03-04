7c65f7f7071f642a5010b82562017d94b27b6b78
Tiago Alves <tiago2018cel123@gmail.com>
2025-03-03 22:19:43 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,27 +1,18 @@
 const script_do_google =
   "https://script.google.com/macros/s/AKfycbzedsZjBBMIh2GGXtocaVloJFVR-SYBi43d_Je1MROGxv34sz98YuDlg67Mg1VIktIi/exec";
-const form = document.forms["form-product"];
+const dados_do_formulario = document.forms['form-product'];
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+dados_do_formulario.addEventListener('submit', function (e) {
+    e.preventDefault();
 
-  if (!form.checkValidity()) {
-    alert("Please fill out all required fields.");
-    return;
-  }
-
-  fetch(script_do_google, {
-    method: "POST",
-    body: new FormData(form),
-    mode: "no-cors",
-  })
-    .then((response) => {
-      alert("DEU BOM PAPAI");
-    })
-    .then(() => {
-      window.location.reload();
-    })
-    .catch((error) => {
-      console.error("Error!", error.message);
-    });
+    fetch(script_do_google, { method: 'POST', body: new FormData(dados_do_formulario) })
+        .then(response => {
+            // Se os dados forem gravados corretamente, será enviada uma mensagem de sucesso
+            alert('Dados enviados com sucesso!', response);
+            dados_do_formulario.reset(); 
+        })
+        .catch(error => {
+            // Se houver erro no envio, a menssagem abaixo será exibida
+            console.error('Erro no envio dos dados!', error);
+        });
 });
