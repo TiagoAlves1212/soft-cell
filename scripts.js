55af059e5ef173699cd0628773b443b70fb0f59f
Tiago Alves <tiago2018cel123@gmail.com>
2025-03-03 21:33:10 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,32 +1,27 @@
-const scriptURL = "https://script.google.com/macros/s/AKfycbwSEQh-Da5JnzYM9PC0ye5OyEbW9xktsIzKlXKOlvRUqDDBpV9wB0RN75J8lJXAUlsm/exec"
-const form = document.forms['contact-form']
+const script_do_google =
+  "https://script.google.com/macros/s/AKfycbwSEQh-Da5JnzYM9PC0ye5OyEbW9xktsIzKlXKOlvRUqDDBpV9wB0RN75J8lJXAUlsm/exec";
+const form = document.forms["form-product"];
 
-form.addEventListener('submit', e => {
-    e.preventDefault()
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    if (!form.checkValidity()) {
-        alert("Please fill out all required fields.")
-        return
-    }
+  if (!form.checkValidity()) {
+    alert("Please fill out all required fields.");
+    return;
+  }
 
-    const numberField = form["your-number"]
-    if (isNaN(numberField.value) || numberField.value.trim === "") {
-        alert("The 'Number' field must contain only numbers.")
-        numberField.focus()
-        return
-    }
-
-    fetch(scriptURL, {method: "POST", body: new FormData(form) })
+  fetch(script_do_google, {
+    method: "POST",
+    body: new FormData(form),
+    mode: "no-cors",
+  })
     .then((response) => {
-        if (!response.ok) {
-            throw new Error("Network response was not ok.")
-        }
-        alert("Thank you! Your form has been submitted successfully.")
+      alert("DEU BOM PAPAI");
     })
     .then(() => {
-        window.location.reload()
+      window.location.reload();
     })
     .catch((error) => {
-        console.error("Error!", error.message)
-    })
-})
+      console.error("Error!", error.message);
+    });
+});
